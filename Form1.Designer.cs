9471ecce4e5dcca55a047af13f5cc8214c55eea2
Felix-Suen <felix.suen2000@gmail.com>
2019-08-22 11:52:13 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Travel
+{
+    partial class AppName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CityText = new System.Windows.Forms.TextBox();
+            this.CityLabel = new System.Windows.Forms.Label();
+            this.CountryLabel = new System.Windows.Forms.Label();
+            this.CountryText = new System.Windows.Forms.TextBox();
+            this.YearLabel = new System.Windows.Forms.Label();
+            this.YearText = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.History = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(178, 24);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(186, 41);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Travel Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // CityText
+            // 
+            this.CityText.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CityText.Location = new System.Drawing.Point(59, 99);
+            this.CityText.Name = "CityText";
+            this.CityText.Size = new System.Drawing.Size(136, 28);
+            this.CityText.TabIndex = 2;
+            // 
+            // CityLabel
+            // 
+            this.CityLabel.AutoSize = true;
+            this.CityLabel.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CityLabel.Location = new System.Drawing.Point(55, 76);
+            this.CityLabel.Name = "CityLabel";
+            this.CityLabel.Size = new System.Drawing.Size(40, 21);
+            this.CityLabel.TabIndex = 3;
+            this.CityLabel.Text = "City";
+            // 
+            // CountryLabel
+            // 
+            this.CountryLabel.AutoSize = true;
+            this.CountryLabel.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountryLabel.Location = new System.Drawing.Point(55, 138);
+            this.CountryLabel.Name = "CountryLabel";
+            this.CountryLabel.Size = new System.Drawing.Size(69, 21);
+            this.CountryLabel.TabIndex = 5;
+            this.CountryLabel.Text = "Country";
+            // 
+            // CountryText
+            // 
+            this.CountryText.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountryText.Location = new System.Drawing.Point(59, 161);
+            this.CountryText.Name = "CountryText";
+            this.CountryText.Size = new System.Drawing.Size(136, 28);
+            this.CountryText.TabIndex = 4;
+            // 
+            // YearLabel
+            // 
+            this.YearLabel.AutoSize = true;
+            this.YearLabel.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YearLabel.Location = new System.Drawing.Point(55, 200);
+            this.YearLabel.Name = "YearLabel";
+            this.YearLabel.Size = new System.Drawing.Size(44, 21);
+            this.YearLabel.TabIndex = 7;
+            this.YearLabel.Text = "Year";
+            // 
+            // YearText
+            // 
+            this.YearText.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YearText.Location = new System.Drawing.Point(59, 223);
+            this.YearText.Name = "YearText";
+            this.YearText.Size = new System.Drawing.Size(136, 28);
+            this.YearText.TabIndex = 6;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(59, 284);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(136, 31);
+            this.AddButton.TabIndex = 8;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveButton.Location = new System.Drawing.Point(310, 284);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(136, 31);
+            this.RemoveButton.TabIndex = 9;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // History
+            // 
+            this.History.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.History.FormattingEnabled = true;
+            this.History.ItemHeight = 25;
+            this.History.Location = new System.Drawing.Point(248, 85);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(256, 179);
+            this.History.TabIndex = 11;
+            // 
+            // AppName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(552, 360);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.YearLabel);
+            this.Controls.Add(this.YearText);
+            this.Controls.Add(this.CountryLabel);
+            this.Controls.Add(this.CountryText);
+            this.Controls.Add(this.CityLabel);
+            this.Controls.Add(this.CityText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Title);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AppName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CityText;
+        private System.Windows.Forms.Label CityLabel;
+        private System.Windows.Forms.Label CountryLabel;
+        private System.Windows.Forms.TextBox CountryText;
+        private System.Windows.Forms.Label YearLabel;
+        private System.Windows.Forms.TextBox YearText;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.ListBox History;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Travel
+{
+    public partial class AppName : Form
+    {
+        public AppName()
+        {
+            InitializeComponent();
+        }
+
+        List<Location> locations = new List<Location>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            if (!Directory.Exists(path = "C:\\Users\\waiho\\source\\repos\\Travel\\Locations - Felix"))
+            {
+                Directory.CreateDirectory(path = "C:\\Users\\waiho\\source\\repos\\Travel\\Locations - Felix");
+            }
+
+            if (!File.Exists(path = "C:\\Users\\waiho\\source\\repos\\Travel\\Locations - Felix\\settings.xml"))
+            {
+                File.Create(path = "C:\\Users\\waiho\\source\\repos\\Travel\\Locations - Felix\\settings.xml"); 
+            }
+            
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Location p = new Location();
+            p.City = CityText.Text;
+            p.Country = CountryText.Text;
+            p.Year = Int32.Parse(YearText.Text);
+            locations.Add(p);
+            History.Items.Add(string.Format("{0} | {1}, {2}", p.Year, p.City, p.Country));
+            CityText.Clear();
+            CountryText.Clear();
+            YearText.Clear();
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    class Location
+    {
+        public string City
+        {
+            get;
+            set;
+        }
+        public string Country
+        {
+            get;
+            set;
+        }
+        public int Year
+        {
+            get;
+            set;
+        }
+    }
+}
